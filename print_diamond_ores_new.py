d5023d422b030c3694a2e7b5d22448ed211a3fb0
F. Chan <edu.fechan@gmail.com>
2020-06-09 13:41:12 -0700

    use 4 bits as min block size

--- print_diamond_ores_new.py
+++ print_diamond_ores_new.py
@@ -6,6 +6,8 @@ import math;
 
 import overviewernbt
 
+SEARCH_BLOCK = "minecraft:obsidian" #the block name you want to print the positions of
+
 #Utility functions
 def unstream(bits_per_value, word_size, data):
     """Converts data in an NBT long array into a list of ints"""
@@ -23,19 +25,32 @@ def unstream(bits_per_value, word_size, data):
                 bl = 0
     return decoded
 
-region = overviewernbt.load_region(filedialog.askopenfile(mode='rb'))
+#Main stuff
+def scan_section(section, search_block):
+    palette = section['Palette']
+    palette_bit_size = math.ceil(math.log2(len(palette))) #number of bits needed to store highest palette index
+    states = unstream(4 if palette_bit_size <= 4 else palette_bit_size, 64, section['BlockStates']) #block indeces are AT LEAST 4 bits
+
+    #coordinates of the chunk at its lowest (x,y,z)
+    chunk_x = chunk[1]['Level']['xPos']*16
+    chunk_y = 16*section['Y']
+    chunk_z = chunk[1]['Level']['zPos']*16
+
+    for y in range(16):
+        for x in range(16):
+            for z in range(16):
+                block_index = y*16*16 + z*16 + x
+                if palette[states[block_index]]['Name'] == search_block:
+                    print(chunk_x + x, chunk_y + y, chunk_z + z, palette[states[block_index]])
+
+region = overviewernbt.load_region(filedialog.askopenfilename())
 for chunkx in range(32):
     for chunkz in range(32):
         try:
             chunk = region.load_chunk(chunkx, chunkz)
-            palette = chunk[1]['Level']['Sections'][1]['Palette']
-            states = unstream(math.ceil(math.log2(len(palette))), 64, chunk[1]['Level']['Sections'][1]['BlockStates'])
-            for y in range(16):
-                for x in range(16):
-                    for z in range(16):
-                        block_index = y*16*16 + z*16 + x
-                        if palette[states[block_index]]['Name'] == "minecraft:obsidian":
-                            print(chunk[1]['Level']['xPos']*16+x, y, chunk[1]['Level']['zPos']*16+z, palette[states[block_index]])
+            for section in chunk[1]['Level']['Sections']:
+                if "Palette" in section:
+                    if SEARCH_BLOCK in [block['Name'] for block in section['Palette']]: #only scan section if desired block is in its palette
+                        scan_section(section, SEARCH_BLOCK)
         except Exception as e:
-            if str(e) == "Unexistent chunk":
-                print("Can't load chunk at", chunkx, chunkz)
+            print(e)
