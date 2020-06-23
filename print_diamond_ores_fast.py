d726859a4be0aad25538e84ee4facf85bb8c0ac9
F. Chan <edu.fechan@gmail.com>
2020-06-23 13:07:03 -0700

    support 1.16 BlockStates

--- print_diamond_ores_fast.py
+++ print_diamond_ores_fast.py
@@ -8,9 +8,12 @@ import overviewernbt
 
 SEARCH_BLOCK = "minecraft:diamond_ore" #the block name you want to print the positions of
 
+NETHER_BLOCKSTATES_DATAVERSION = 2529 #minimum DataVersion using the 1.16 BlockStates format (20w17a)
+
 #Utility functions
 def unstream(bits_per_value, word_size, data):
-    """Converts data in an NBT long array into a list of ints"""
+    """Converts data in an NBT long array into a list of ints, used before the introduction of the
+    new BlockStates format introduced in 20w19a"""
     bl = 0
     v = 0
     decoded = []
@@ -25,11 +28,32 @@ def unstream(bits_per_value, word_size, data):
                 bl = 0
     return decoded
 
+def nether_unstream(bits_per_value, data):
+    """Converts data in an NBT long array into a list of ints, used after the introduction of the
+    new BlockStates format introduced in 20w19a and used in the 1.16 Nether Update (the one with 0s
+    padding each LONG if the size (bits) isn't a multiple of 64).
+    
+    Adapted from Minecraft Overviewer's overviewer_core/world.py, but doesn't depend on numpy"""
+    n = 4096
+    result = [0] * n
+    shorts_per_long = 64 // bits_per_value
+    mask = (1 << bits_per_value) - 1
+
+    for i in range(shorts_per_long):
+        j = (n + shorts_per_long - 1 - i) // shorts_per_long
+        result[i::shorts_per_long] = [(b >> (bits_per_value * i)) & mask for b in data[:j]]
+    
+    return result
+
 #Main stuff
 def scan_section(section, search_block):
     palette = section['Palette']
     palette_bit_size = math.ceil(math.log2(len(palette))) #number of bits needed to store highest palette index
-    states = unstream(4 if palette_bit_size <= 4 else palette_bit_size, 64, section['BlockStates']) #block indeces are AT LEAST 4 bits
+    palette_bit_size = 4 if palette_bit_size <= 4 else palette_bit_size #block indeces are AT LEAST 4 bits
+    if chunk[1]['DataVersion'] < NETHER_BLOCKSTATES_DATAVERSION:
+        states = unstream(palette_bit_size, 64, section['BlockStates']) 
+    else:
+        states = nether_unstream(palette_bit_size, section['BlockStates'])
 
     #coordinates of the chunk at its lowest (x,y,z)
     chunk_x = chunk[1]['Level']['xPos']*16
